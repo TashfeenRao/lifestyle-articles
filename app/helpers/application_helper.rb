e771461d65a0f7f8c44eba9b76ef139aee577354
TashfeenRao <raotashfeen25@gmail.com>
2020-03-25 22:00:01 +0500

    responsive nabar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
     def vote_devote(article)
         vote = Vote.find_by(user_id: current_user.id,article_id:article.id)
         if vote
-            button_to "Devote", {controller: "votes", action: "destroy",id: vote.id,article_id: article.id },method: :delete , class:"btn btn-primary"
+            button_to "Devote", {controller: "votes", action: "destroy",id: vote.id,article_id: article.id },method: :delete , class:"btn btn-danger"
         else
          button_to "Vote", {controller: "votes", action: "create",article_id: article.id },method: :post , class:"btn btn-primary"
         end
